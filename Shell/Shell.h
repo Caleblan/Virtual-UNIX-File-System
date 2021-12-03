ee5e3d2d196f24bbd25b41d14993e4f544611098
Caleb Landry <clandry@unbc.ca>
2021-12-02 17:05:22 -0800

    Add new file

---
+++ Shell/Shell.h
@@ -0,0 +1,62 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+/**
+ * Reads input from stdin one character at a time and dynamically
+ * allocates enough memory to store command. 
+ * 
+ * Note: This function does not make null terminated strings at this point
+ *      as due to realloc, the address may change so we need to store the 
+ *      whole string initially before altering it.
+ * 
+ * @return Complete command entered by user. 
+ */
+char *getUserInput(void);
+
+/**
+ * Takes the inputed string by user and creates a list of null terminated strings.
+ * 
+ * Note: This done in place of the original commands allocated memory area, therefore
+ *       altering the original input string.
+ * 
+ * @parsedCommandPtr Pointer to all the address' containing each word in
+ * user input.
+ * 
+ * @return An array of addresses pointing to the beginning of each now-null-terminated word in
+ * the user inputed comamand. 
+ */
+char **tokenize(char **command);
+
+/**
+ * Creates a child process by system call to run executable,
+ * which the parent program will wait until the childs completion.
+ * 
+ * @parsedCommandPtr Pointer to all the address' containing each word in
+ * user input.
+ */
+void executeFile(char ***parsedCommandPtr);
+
+/**
+ * Parses user command to a command.
+ * 
+ * @parsedCommandPtr Pointer to all the addresses containing each word in
+ * user input.
+ */
+void parseCommand(char ***parsedCommandPtr);
+
+/**
+ * Free's both the user input command as well as as the pointer containing all the
+ * addresses to the beginning of each words first character.
+ * 
+ * Note: When calling free(ptr) for actual input string, we only need to 
+ *       give the method the first arguement. This is because when we tokenize
+ *       the user string, it is done in place of orignal allocated memory location,
+ *       and since technically it is only a single allocation (with reallocations), 
+ *       we only have to free once.
+ * 
+ * @parsedCommandPtr Pointer to all the addresses containing each word in
+ * user input.
+ */
+void freeMemory(char ***parsedCommandPtr);
+
+#endif
