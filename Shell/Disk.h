3b210968acadabc33edab36f4f003cba8c20b8c6
Caleb Landry <clandry@unbc.ca>
2021-12-04 12:12:07 -0800

    Add new file

---
+++ Shell/Disk.h
@@ -0,0 +1,15 @@
+#ifndef DISK_H
+#define DISK_H
+
+#define BLOCK_SIZE 128;
+
+#include <stddef.h>
+
+unsigned int diskSize = 0;
+unsigned int diskBlocks = 0;
+
+char *disk2 = NULL;
+
+void diskWrite(unsigned int diskLocation, char** blockData);
+
+void diskRead(unsigned int diskLocation);
