8f013468ab1eda5e0f31ac7b48c98b8a01c4b0b8
Caleb Landry <clandry@unbc.ca>
2021-12-04 14:18:43 -0800

    Update Shell.c

--- Shell/Shell.c
+++ Shell/Shell.c
@@ -13,6 +13,14 @@ bool errorFound = false;
 //Used to continuously prompt user until stated otherwise.
 bool active = true;
 
+
+static unsigned int diskSize = 0;
+static unsigned int diskBlocks = 0;
+
+static char *disk2 = NULL;
+
+
+
 /**
  * Reads input from stdin one character at a time and dynamically
  * allocates enough memory to store command. 
