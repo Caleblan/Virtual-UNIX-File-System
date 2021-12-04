5be4c6089bc3ba61a8b460e53c2ddfaf4eac5c92
Caleb Landry <clandry@unbc.ca>
2021-12-04 12:40:30 -0800

    Update Disk.h

--- Shell/Disk.h
+++ Shell/Disk.h
@@ -1,82 +1,15 @@
-#ifndef SHELL_H
-#define SHELL_H
+#ifndef DISK_H
+#define DISK_H
 
-#define MAGIC_NUMBER 0xf0f03410
-#define BLOCKGROUP_SIZE 1024
+#define BLOCK_SIZE 128
 
-/**
- * Reads input from stdin one character at a time and dynamically
- * allocates enough memory to store command. 
- * 
- * Note: This function does not make null terminated strings at this point
- *      as due to realloc, the address may change so we need to store the 
- *      whole string initially before altering it.
- * 
- * @return Complete command entered by user. 
- */
-char *getUserInput(void);
+#include <stddef.h>
 
-/**
- * Takes the inputed string by user and creates a list of null terminated strings.
- * 
- * Note: This done in place of the original commands allocated memory area, therefore
- *       altering the original input string.
- * 
- * @parsedCommandPtr Pointer to all the address' containing each word in
- * user input.
- * 
- * @return An array of addresses pointing to the beginning of each now-null-terminated word in
- * the user inputed comamand. 
- */
-char **tokenize(char **command);
+unsigned int diskSize = 0;
+unsigned int diskBlocks = 0;
 
-/**
- * Creates a child process by system call to run executable,
- * which the parent program will wait until the childs completion.
- * 
- * @parsedCommandPtr Pointer to all the address' containing each word in
- * user input.
- */
-void executeFile(char ***parsedCommandPtr);
+char *disk2 = NULL;
 
-/**
- * Parses user command to a command.
- * 
- * @parsedCommandPtr Pointer to all the addresses containing each word in
- * user input.
- */
-void parseCommand(char ***parsedCommandPtr);
+void diskWrite(unsigned int diskLocation, char** blockData);
 
-/**
- * Free's both the user input command as well as as the pointer containing all the
- * addresses to the beginning of each words first character.
- * 
- * Note: When calling free(ptr) for actual input string, we only need to 
- *       give the method the first arguement. This is because when we tokenize
- *       the user string, it is done in place of orignal allocated memory location,
- *       and since technically it is only a single allocation (with reallocations), 
- *       we only have to free once.
- * 
- * @parsedCommandPtr Pointer to all the addresses containing each word in
- * user input.
- */
-void freeMemory(char ***parsedCommandPtr);
-
-
-
-int diskWriteCommand(char ***parsedCommandPtr);
-
-int diskReadCommand(char ***parsedCommandPtr);
-
-int writeFile(char ***parsedCommandPtr);
-
-int deleteFile(char ***parsedCommandPtr);
-
-int makeFile(char ***parsedCommandPtr);
-
-int createDirectory(char ***parsedCommandPtr);
-
-void formatDisk();
-
-
-#endif
+void diskRead(unsigned int diskLocation);
