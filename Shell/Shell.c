c35ea95bcdfe7052f0a92462df8bc8881fdc1f7b
Caleb Landry <clandry@unbc.ca>
2021-12-04 16:01:12 -0800

    Update Shell.c

--- Shell/Shell.c
+++ Shell/Shell.c
@@ -315,6 +315,8 @@ void createDisk(char ***parsedCommandPtr)
 
     //Used to store an array of structs.
     disk2 = calloc(diskSize, sizeof(char));
+
+    printf("Disk partition of size %d bytes has been created (BlockSize: %d, BlockCount: %d).\n", diskSize, diskBlocks);
 }
 
 
