946d9b32b0ddfbc6437d087b803f259e2391cd70
Caleb Landry <clandry@unbc.ca>
2021-12-07 19:26:54 -0800

    Update Shell.c

--- Shell/Shell.c
+++ Shell/Shell.c
@@ -487,14 +487,14 @@ void makeFile(char ***parsedCommandPtr)
     
     //Splits the inode count (int) into four bytes.
     
-    inodeCount += (metaData[8] << 24);
-    inodeCount += (metaData[9] << 16);
-    inodeCount += (metaData[10] << 8);
+    // inodeCount += (metaData[8] << 24);
+    // inodeCount += (metaData[9] << 16);
+    // inodeCount += (metaData[10] << 8);
     inodeCount += metaData[11];
 
-    printf("%d.\n", metaData[8] << 24);
-    printf("%d.\n", metaData[9] << 16);
-    printf("%d.\n", metaData[10] << 8);
+    // printf("%d.\n", metaData[8] << 24);
+    // printf("%d.\n", metaData[9] << 16);
+    // printf("%d.\n", metaData[10] << 8);
     printf("%d.\n", metaData[11]);
 
     //If no inode is availble, notify user.
